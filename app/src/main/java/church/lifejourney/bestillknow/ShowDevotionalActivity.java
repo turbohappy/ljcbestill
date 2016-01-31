20a795e62f5b4eebdd5ec034dd9350ff4c3ee7e8
Brian Davis <turbohappy@gmail.com>
2016-01-31 15:06:31 -0500

    working awesome, cut off

--- app/src/main/java/church/lifejourney/bestillknow/ShowDevotionalActivity.java
+++ app/src/main/java/church/lifejourney/bestillknow/ShowDevotionalActivity.java
@@ -1,15 +1,17 @@
 package church.lifejourney.bestillknow;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.TextView;
 
-import church.lifejourney.bestillknow.download.HtmlTask;
+import org.xml.sax.XMLReader;
+
+import church.lifejourney.bestillknow.helper.Logger;
 
 public class ShowDevotionalActivity extends AppCompatActivity {
 
@@ -22,11 +24,26 @@ public class ShowDevotionalActivity extends AppCompatActivity {
 
         TextView contentView = (TextView) findViewById(R.id.dev_content_view);
 
-        String url = getIntent().getStringExtra("url");
-        new HtmlTask(contentView).execute(url);
+        String content = getIntent().getStringExtra("content");
+        content = content.replaceAll("<[/]*em>", "");
+        contentView.setText(Html.fromHtml(content, new DevotionalImageGetter(), new DevotionalTagHandler()));
+        contentView.setMovementMethod(new ScrollingMovementMethod());
+    }
 
-//        String content = getIntent().getStringExtra("content");
-//        contentView.setText(Html.fromHtml(content));
-//        contentView.setMovementMethod(new ScrollingMovementMethod());
+    private static class DevotionalTagHandler implements Html.TagHandler {
+
+        @Override
+        public void handleTag(boolean opening, String tag, Editable output, XMLReader xmlReader) {
+            Logger.debug(this, "need to handle tag [" + tag + "] [opening=" + opening + "]");
+        }
+    }
+
+    private static class DevotionalImageGetter implements Html.ImageGetter {
+
+        @Override
+        public Drawable getDrawable(String source) {
+            Logger.debug(this, "need to handle image [" + source + "]");
+            return null;
+        }
     }
 }
