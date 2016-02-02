bfc753185b1355a8cb404910ba81b76f91f856a5
Brian Davis <turbohappy@gmail.com>
2016-02-01 23:28:22 -0500

    remove passage activity

--- app/src/main/java/church/lifejourney/bestillknow/download/LoadPassageTask.java
+++ app/src/main/java/church/lifejourney/bestillknow/download/LoadPassageTask.java
@@ -1,7 +1,5 @@
 package church.lifejourney.bestillknow.download;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.text.Html;
 import android.widget.TextView;
@@ -10,8 +8,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import church.lifejourney.bestillknow.activity.PassageActivity;
-import church.lifejourney.bestillknow.activity.ShowDevotionalActivity;
 import church.lifejourney.bestillknow.helper.Logger;
 
 /**
@@ -19,29 +15,29 @@ import church.lifejourney.bestillknow.helper.Logger;
  */
 public class LoadPassageTask extends AsyncTask<String, Void, String> {
 
-    private TextView contentView;
+	private TextView contentView;
 
-    public LoadPassageTask(TextView contentView) {
-        this.contentView = contentView;
-    }
+	public LoadPassageTask(TextView contentView) {
+		this.contentView = contentView;
+	}
 
-    private Exception exception;
+	private Exception exception;
 
-    protected String doInBackground(String... urls) {
-        try {
-            String url = urls[0];
-            Logger.debug(this, "Loading passage from " + url);
-            Document doc = Jsoup.connect(url).get();
-            Element el = doc.select("div.result-text-style-normal.text-html").first();
-            return el.outerHtml();
-        } catch (Exception e) {
-            this.exception = e;
-            return null;
-        }
-    }
+	protected String doInBackground(String... urls) {
+		try {
+			String url = urls[0];
+			Logger.debug(this, "Loading passage from " + url);
+			Document doc = Jsoup.connect(url).get();
+			Element el = doc.select("div.result-text-style-normal.text-html").first();
+			return el.outerHtml();
+		} catch (Exception e) {
+			this.exception = e;
+			return null;
+		}
+	}
 
-    protected void onPostExecute(String html) {
-        // TODO: check this.exception
-        contentView.setText(Html.fromHtml(html));
-    }
+	protected void onPostExecute(String html) {
+		// TODO: check this.exception
+		contentView.setText(Html.fromHtml(html));
+	}
 }
