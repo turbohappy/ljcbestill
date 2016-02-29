ea85555f4f4b69040afebbbc7e11cd0a2d9de3e2
Brian Davis <turbohappy@gmail.com>
2016-02-29 00:37:37 -0500

    Change styling to fix bug on older devices

--- app/src/main/java/church/lifejourney/bestillknow/db/DevotionalDb.java
+++ app/src/main/java/church/lifejourney/bestillknow/db/DevotionalDb.java
@@ -19,90 +19,92 @@ import io.realm.Sort;
  * Created by bdavis on 2/3/16.
  */
 public class DevotionalDb {
-	private Realm realm;
+    private Realm realm;
 
-	public DevotionalDb() {
-		this.realm = Realm.getDefaultInstance();
-	}
+    public DevotionalDb() {
+        this.realm = Realm.getDefaultInstance();
+    }
 
-	public DevotionalDb(Context context) {
-		this.realm = Realm.getInstance(Application.realmConfig(context));
-	}
+    public DevotionalDb(Context context) {
+        this.realm = Realm.getInstance(Application.realmConfig(context));
+    }
 
-	public void addChangeListener(RealmChangeListener listener) {
-		realm.addChangeListener(listener);
-	}
+    public void addChangeListener(RealmChangeListener listener) {
+        realm.addChangeListener(listener);
+    }
 
-	public void close() {
-		this.realm.close();
-	}
+    public void close() {
+        this.realm.close();
+    }
 
-	public List<Devotional> readDevotionals() {
-		return realm.where(Devotional.class).findAllSorted("pubDate", Sort.DESCENDING);
-	}
+    public List<Devotional> readDevotionals() {
+        return realm.where(Devotional.class).findAllSorted("pubDate", Sort.DESCENDING);
+    }
 
-	public List<Devotional> readUnreadDevotionals() {
-		return realm.where(Devotional.class).equalTo("unread", true).findAll();
-	}
+    public List<Devotional> readUnreadDevotionals() {
+        return realm.where(Devotional.class).equalTo("unread", true).findAll();
+    }
 
-	public Devotional readDevotional(String guid) {
-		RealmResults<Devotional> results = realm.where(Devotional.class)
-				.equalTo("guid", guid).findAll();
-		if (results == null || results.size() == 0) {
-			return null;
-		} else if (results.size() == 1) {
-			return results.get(0);
-		} else {
-			throw new RuntimeException("There should never be multiple " +
-					"entries with the same guid");
-		}
-	}
+    public Devotional readDevotional(String guid) {
+        RealmResults<Devotional> results = realm.where(Devotional.class)
+                .equalTo("guid", guid).findAll();
+        if (results == null || results.size() == 0) {
+            return null;
+        } else if (results.size() == 1) {
+            return results.get(0);
+        } else {
+            throw new RuntimeException("There should never be multiple " +
+                    "entries with the same guid");
+        }
+    }
 
-	public void markDevotionalRead(Devotional devotional) {
-		realm.beginTransaction();
-		devotional.setUnread(false);
-		realm.commitTransaction();
-	}
+    public void markDevotionalRead(Devotional devotional) {
+        realm.beginTransaction();
+        devotional.setUnread(false);
+        realm.commitTransaction();
+    }
 
-	@NonNull
-	public List<Devotional> saveIfNew(List<RSSItem> items, boolean markUnread) {
-		List<Devotional> written = new ArrayList<>();
+    @NonNull
+    public List<Devotional> saveIfNew(List<RSSItem> items, boolean markUnread) {
+        List<Devotional> written = new ArrayList<>();
 
-		realm.beginTransaction();
+        realm.beginTransaction();
 
-		for (RSSItem item : items) {
-			Devotional devotional = readDevotional(item.getGuid());
+        for (RSSItem item : items) {
+            Devotional devotional = readDevotional(item.getGuid());
 
-			if (devotional == null) {
-				written.add(parseDevotionalAndWriteToDbWithoutCommitting(item, markUnread));
-			}
-		}
+            if (devotional == null) {
+                written.add(parseDevotionalAndWriteToDbWithoutCommitting(item, markUnread));
+            }
+        }
 
-		if (written.size() > 0) {
-			realm.commitTransaction();
-		} else {
-			realm.cancelTransaction();
-		}
+        if (written.size() > 0) {
+            realm.commitTransaction();
+        } else {
+            realm.cancelTransaction();
+        }
 
-		return written;
-	}
+        return written;
+    }
 
-	private Devotional parseDevotionalAndWriteToDbWithoutCommitting(RSSItem item, boolean markUnread) {
-		Devotional devUnwritten = new DevotionalParser().parse(item);
-		devUnwritten.setUnread(markUnread);
+    private Devotional parseDevotionalAndWriteToDbWithoutCommitting(RSSItem item, boolean markUnread) {
+        Devotional devUnwritten = new DevotionalParser().parse(item);
+        devUnwritten.setUnread(markUnread);
 
-		return realm.copyToRealm(devUnwritten);
-	}
+        return realm.copyToRealm(devUnwritten);
+    }
 
-	public void deleteFirstXDevotionals(int number) {
-		realm.beginTransaction();
+    public void deleteFirstXDevotionals(int number) {
+        realm.beginTransaction();
 
-		RealmResults<Devotional> devotionals = realm.where(Devotional.class).findAllSorted("pubDate", Sort.DESCENDING);
-		for (int i = 0; i < number; i++) {
-			devotionals.first().removeFromRealm();
-		}
+        RealmResults<Devotional> devotionals = realm.where(Devotional.class).findAllSorted("pubDate", Sort.DESCENDING);
+        if (devotionals.size() >= number) {
+            for (int i = 0; i < number; i++) {
+                devotionals.first().removeFromRealm();
+            }
+        }
 
-		realm.commitTransaction();
-		Logger.debug(this, String.format("Removed first %s devotionals", number));
-	}
+        realm.commitTransaction();
+        Logger.debug(this, String.format("Removed first %s devotionals", number));
+    }
 }
